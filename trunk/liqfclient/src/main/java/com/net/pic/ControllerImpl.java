a982768776e7acaa1f0259e94d37fdbd2bc31596
scholers@gmail.com <scholers@gmail.com@d8aaa172-7280-dbad-ac2c-a30a0d501d70>
2012-03-25 10:27:42 +0000

    单个URL下载代码OK

--- trunk/liqfclient/src/main/java/com/net/pic/ControllerImpl.java
+++ trunk/liqfclient/src/main/java/com/net/pic/ControllerImpl.java
@@ -5,9 +5,11 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CyclicBarrier;
 
 import javax.swing.JTextArea;
 
+import com.net.pic.ui.HttpClientUrl;
 import com.net.pic.ui.MainWin;
 
 public class ControllerImpl implements Controller {
@@ -15,24 +17,39 @@ public class ControllerImpl implements Controller {
     private JTextArea messageArea;
     private DataFetcher fetcher = new DataFetcherImpl();
     private DataHandler hander = new DataHandlerImpl();
+    private DataHandler urlHander = new UrlHandlerImpl();
+    private final static String PRX_URL = "http://www.xfjiayuan.com/";
 
     public ControllerImpl(MainWin mainWin) {
         this.mainWin = mainWin;
         this.messageArea = mainWin.getMessageArea();
     }
 
+    public ControllerImpl() {
 
+    }
+    
     public List<File> fetchImages(String pageUrl, String imgSaveDir)
             throws MalformedURLException, IOException {
-        
+    	HttpClientUrl clintUrl = new HttpClientUrl();
         // 获取html页面
-        StringBuffer page = fetcher.fetchHtml(pageUrl);
+        StringBuffer page = fetcher.fetchHtml(pageUrl, clintUrl);
 
         // 获取页面中的地址
         List<String> imgUrls = hander.getImageUrls(page);
-
+        
+        if(imgUrls.size() <= 0) {
+        	List<String> urlStrs =  urlHander.getUrls(page);
+        	for(String tempUrl : urlStrs) {
+        		
+        		page = fetcher.fetchHtml(PRX_URL + tempUrl, clintUrl);
+        		imgUrls.addAll(hander.getImageUrls(page));
+        	}
+        }
+        System.out.println(imgUrls.toString());
         // 保存图片，返回文件列表
         List<File> fileList = new ArrayList<File>();
+        /*
         int i = 1;
         for (String url : imgUrls) {
             File file = fetcher.fecthFile(url, imgSaveDir + "//" + i + ".jpg");
@@ -44,12 +61,37 @@ public class ControllerImpl implements Controller {
             mainWin.update(mainWin.getGraphics());
             fileList.add(file);
             i++;
+        }*/
+        //多线程下载
+        CyclicBarrier cb = new CyclicBarrier(imgUrls.size()); 
+        int i = 1;
+        for (String url : imgUrls) {
+        	String newFileName = "00" + i +".jpg";
+        	String threadName = "d" + i;
+        	new Downloader(threadName, url, newFileName, imgSaveDir, cb, messageArea).start();
+        	 //fileList.add(file);
+             i++;
         }
-        
+        if(messageArea != null)
         messageArea.setText(
-                messageArea.getText() + "/n" + " 任务完成，共下载"+fileList.size()+"个图片!");
+                messageArea.getText() + "/n" + " 任务完成，共下载"+i+"个图片!");
         
         return fileList;
+    }
+    
+    public static void main(String[] args) {
+    	Controller controller = new ControllerImpl();
+        String testUrl = "http://www.xfjiayuan.com/forum-25-1.html";
+        //二级解析
+        try {
+			List<File> fileList = controller.fetchImages(testUrl, "d://pic//");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
 }
